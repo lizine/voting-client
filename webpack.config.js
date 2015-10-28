8f2dd61ef000e2dd4d83b4a351c422435ea3209f
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-10-28 15:05:59 +0200

    adding webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	entry: [
+	 './src/index.js'
+	],
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	}
+
+};
