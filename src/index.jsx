cb9640a829820c3a61b6a81ccd3bb186db53c627
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-10-30 16:20:02 +0200

    removed placeholder data, changed renderfunction

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Route} from 'react-router';
+import Router, {Route} from 'react-router';
 import App from './components/App'
 import Voting from './components/Voting';
-
-const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
 <Route path="/" component={Voting}/>
@@ -12,6 +10,6 @@ const routes = <Route component={App}>
 
 
 ReactDom.render(
-	<Voting pair={pair} winner="Trainspotting" />,
-	document.getElementById('app')
+	<Router>{routes}</Router>,
+	document.getElementById('app');
 );
