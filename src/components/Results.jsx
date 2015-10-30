f2000e59f39dbc2ea6280d5c9a11af883e21bb7f
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-10-30 16:42:13 +0200

    results now shows titles and votes

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,26 @@ import PureRenderinMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
 	mixins: [PureRenderinMixin],
+
+	getPair: function(){
+		return this.props.pair || [];
+	},
+	getVotes: function(entry){
+		if(this.props.tally && this.props.tally.has(entry)){
+			return this.props.tally.get(entry);
+		}
+		return 0;
+	},
 	render: function() {
-		return <div>Hello from results!</div>
+		return <div className="results">
+		{this.getPair().map(entry =>
+			<div key={entry} className="entry">
+			<h1>{entry}</h1>
+			<div className="voteCount">
+			{this.getVotes(entry)}
+			</div>
+			</div>
+        )}
+			</div>;
 	}
 });
