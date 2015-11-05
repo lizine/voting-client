80a9bf727a08a1742aebb65a05cd5d08515b864e
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 10:49:26 +0200

    next button added for results view

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -15,6 +15,7 @@ export default React.createClass({
 	},
 	render: function() {
 		return <div className="results">
+		<div className="tally">
 		{this.getPair().map(entry =>
 			<div key={entry} className="entry">
 			<h1>{entry}</h1>
@@ -23,6 +24,14 @@ export default React.createClass({
 			</div>
 			</div>
         )}
-			</div>;
+			</div>
+			<div className="management">
+			<button ref="next"
+			className="next"
+			onClick={this.props.next}>
+			Next
+			</button>
+			</div>
+			</div>
 	}
 });
