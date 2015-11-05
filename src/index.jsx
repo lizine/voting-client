03c23166e8e3caf952767b1ec4d95d8fb5f81a5e
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 13:42:17 +0200

    adding reducer.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,15 @@ import ReactDom from 'react-dom';
 import Router, {Route} from 'react-router';
 import App from './components/App'
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const routes = <Route component={App}>
+<Route path="/results" component={Results} />
 <Route path="/" component={Voting}/>
 </Route>;
 
 
 ReactDom.render(
 	<Router>{routes}</Router>,
-	document.getElementById('app');
+	document.getElementById('app')
 );
---
+++ src/reducer.js
@@ -0,0 +1,6 @@
+import {Map} from 'immutable';
+
+export default function(state = Map(), action){
+
+	return state;
+}
