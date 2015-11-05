87e2d318986147956d129b3c43cb1f6fb9e691ba
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 16:23:29 +0200

    connecting hasVoted entry to the props on voting

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -19,6 +19,7 @@ export const Voting = React.createClass({
 function mapStateToProps(state){
 	return {
 		pair: state.getIn(['vote', 'pair']),
+		hasVoted: state.get('hasVoted'),
 		winner: state.get('winner')
 	};
 
