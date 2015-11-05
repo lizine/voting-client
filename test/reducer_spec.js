ed48ef4ca8bf3b3a303aa14ba6e524b26bdb5d4a
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 16:24:35 +0200

    added tests for voting

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,5 +1,5 @@
 import {List, Map, fromJS} from 'immutable';
-import { expect} from 'chai';
+import {expect} from 'chai';
 
 import reducer from '../src/reducer';
 
@@ -10,8 +10,8 @@ describe('reducer', () => {
 		const action = {
 			type: 'SET_STATE',
 			state: Map({
-				vote:Map({
-					pair: List.of('Trainsportting', '28 Days Later'),
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
 					tally: Map({Trainspotting:1})
 				})
 			})
@@ -25,7 +25,7 @@ describe('reducer', () => {
 			}
 		}));
 
-	})
+	});
 	it('handles SET_STATE with plain JS payload', () => {
 		const initialState = Map();
 		const action = {
@@ -58,6 +58,7 @@ describe('reducer', () => {
 			}
 		};
 		const nextState = reducer(undefined, action);
+
 		expect(nextState).to.equal(fromJS({
 			vote:{
 				pair: ['Trainspotting', '28 Days Later'],
@@ -65,5 +66,65 @@ describe('reducer', () => {
 			}
 		}));
 	});
+
+	it('handles VOTE by setting hasVoted', () => {
+		const state = fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 Days Later'],
+				tally: {Trainspotting: 1}
+			}
+		});
+		const action = {type: 'VOTE', entry: 'Trainspotting'};
+		const nextState = reducer(state, action);
+
+		expect(nextState).to.equal(fromJS({
+			vote: {
+					pair: ['Trainspotting', '28 Days Later'],
+				tally: {Trainspotting: 1}
+			},
+			hasVoted: 'Trainspotting'
+		}));
+	});
+	it('does not set hasVoted for VOTE on invalid entry', () => {
+  const state = fromJS({
+    vote: {
+      pair: ['Trainspotting', '28 Days Later'],
+      tally: {Trainspotting: 1}
+    }
+  });
+  const action = {type: 'VOTE', entry: 'Sunshine'};
+  const nextState = reducer(state, action);
+
+  expect(nextState).to.equal(fromJS({
+    vote: {
+      pair: ['Trainspotting', '28 Days Later'],
+      tally: {Trainspotting: 1}
+    }
+  }));
+});
+	it('removes hasVoted on SET_STATE if pair changes', () => {
+  const initialState = fromJS({
+    vote: {
+      pair: ['Trainspotting', '28 Days Later'],
+      tally: {Trainspotting: 1}
+    },
+    hasVoted: 'Trainspotting'
+  });
+  const action = {
+    type: 'SET_STATE',
+    state: {
+      vote: {
+        pair: ['Sunshine', 'Slumdog Millionaire']
+      }
+    }
+  };
+  const nextState = reducer(initialState, action);
+
+  expect(nextState).to.equal(fromJS({
+    vote: {
+      pair: ['Sunshine', 'Slumdog Millionaire']
+    }
+  }));
+});
 
 });
