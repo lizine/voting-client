e47ba8f68cbdc52b899a800fd7ebe7fede0fdbbf
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 10:48:49 +0200

    adding tests for next button in results

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React from 'react/addons';
 import {List, Map} from 'immutable';
-import Results from '/../../src/components/Results';
+import Results from '../../src/components/Results';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponensWithClass}
+const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate}
  = React.addons.TestUtils;
 
  describe('Results', () =>{
 
- 	it('renders entries with vote counts or zero' ()=>{
+ 	it('renders entries with vote counts or zero' , () => {
  		const pair = List.of('Trainspotting', '28 Days Later');
  		const tally = Map({'Trainspotting':5});
  		const component = renderIntoDocument(
  			<Results pair={pair} tally={tally} />
  			);
- 		const entries = scryRenderedDOMComponensWithClass(component, 'entry');
+ 		const entries = scryRenderedDOMComponentsWithClass(component, 'entry');
  		const [train, days] = entries.map( e => e.textContent);
 
- 		expect(etnries.length).to.equal(2);
+ 		expect(entries.length).to.equal(2);
  		expect(train).to.contain('Trainspotting');
  		expect(train).to.contain('5');
  		expect(days).to.contain('28 Days Later');
  		expect(days).to.contain('0');
+ 	});
+ 	it('invokes the next callback when next button is clicked', () =>{
+ 		let nextInvoked = false;
+ 		const next = () => nextInvoked = true;
+
+ 		const pair = List.of('Trainspotting', '28 Days Later');
+ 		const component = renderIntoDocument(
+ 			<Results pair={pair}
+ 			tally={Map()}
+ 			next={next}/>
+ 			);
+ 		Simulate.click(React.findDOMNode(component.refs.next));
+
+ 		expect(nextInvoked).to.equal(true);
  	});
 
  });
