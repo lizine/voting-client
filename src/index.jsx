1f9114ab15f5170df51d1117345e3d2c3d613317
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 15:20:00 +0200

    updating routing table to use VotingContainer

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Router, {Route} from 'react-router';
-import App from './components/App'
-import Voting from './components/Voting';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
 import Results from './components/Results';
 
+const store = createStore(reducer);
+store.dispatch({
+	type: 'SET_STATE',
+	state: {
+		vote: {
+			pair: ['Sunshine,', '28 Days Later'],
+			tally:{Sunshine: 2}
+		}
+	}
+});
 const routes = <Route component={App}>
 <Route path="/results" component={Results} />
-<Route path="/" component={Voting}/>
+<Route path="/" component={VotingContainer}/>
 </Route>;
 
 
 ReactDom.render(
-	<Router>{routes}</Router>,
+	<Provider store={store}>
+	<Router>{routes}</Router>
+	</Provider>,
 	document.getElementById('app')
 );
