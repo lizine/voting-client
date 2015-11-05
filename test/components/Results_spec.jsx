080ecfd47d4a96f060a0a2e8300de451d0e1cb8f
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-11-05 11:27:06 +0200

    test for showing winner

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -37,5 +37,16 @@ const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate}
 
  		expect(nextInvoked).to.equal(true);
  	});
+ 	it('renders the winner when there is one', () => {
+ 		const component = renderIntoDocument(
+ 			<Results winner="Trainspotting"
+ 			pair={['Trainspotting', '28 Days Later']}
+ 			tally={Map()} />
+ 			);
+ 		const winner = React.findDOMNode(component.refs.winner);
+ 		expect(winner).to.be.ok;
+ 		expect(winner.textContent).to.contain('Trainspotting');
+
+ 	});
 
  });
